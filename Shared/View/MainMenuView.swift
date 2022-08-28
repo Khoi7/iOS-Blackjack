142d11f333c387a82c18057f894642b3a09886ab
Khoi7 <s3694615@rmit.edu.vn>
2022-08-28 18:50:53 +0700

    Navigation

--- Shared/View/MainMenuView.swift
+++ Shared/View/MainMenuView.swift
@@ -8,23 +8,34 @@
 import SwiftUI
 
 struct MainMenuView: View {
+    @State var colorScheme: ColorScheme = .light
+    @State var darkMode = false
+    
     var body: some View {
-        ZStack{
-            RadialGradient(colors: [Color("Bright Green"), Color("Dark Green")], center: .center, startRadius: 0, endRadius: 400)
-                .ignoresSafeArea()
-            VStack {
-                Text("Blackjack".uppercased())
-                    .foregroundColor(.yellow)
-                    .font(.system(size: 50, weight: .heavy, design: .serif))
-                    .shadow(color: .black, radius: 10, x: 5, y: 5)
-                    .padding(.bottom, 30)
-                Text("New Game")
-                    .modifier(MenuChoiceText())
-                Text("How To Play")
-                    .modifier(MenuChoiceText())
-                Text("Highscores")
-                    .modifier(MenuChoiceText())
+        NavigationView {
+            ZStack{
+                RadialGradient(colors: [Color("Bright Table"), Color("Dark Table")], center: .center, startRadius: 0, endRadius: 400)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Blackjack".uppercased())
+                        .foregroundColor(.yellow)
+                        .font(.system(size: 50, weight: .heavy, design: .serif))
+                        .shadow(color: .black, radius: 10, x: 5, y: 5)
+                        .padding(.bottom, 30)
+                    
+                    NavigationLink(destination: GameView()
+                                    .navigationBarBackButtonHidden(true)) {
+                        Text("New Game")
+                            .modifier(MenuChoiceText())
+                    }
+                    Text("How To Play")
+                        .modifier(MenuChoiceText())
+                    Text("Highscores")
+                        .modifier(MenuChoiceText())
+                }
+
             }
+            .preferredColorScheme(colorScheme)
         }
     }
 }

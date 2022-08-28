49c44fda8d2f02891980445976c9316cbc9aada1
Khoi7 <s3694615@rmit.edu.vn>
2022-08-28 18:51:07 +0700

    Show cards

--- Shared/View/GameView.swift
+++ Shared/View/GameView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct GameView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RadialGradient(colors: [Color("Bright Table"), Color("Dark Table")], center: .center, startRadius: 0, endRadius: 400)
+                .ignoresSafeArea()
+            VStack {
+                // MARK: CPU hand
+                ZStack {
+                    CardView(number: 0, suit: 0)
+                    CardView(number: 0, suit: 0)
+                        .offset(x: 50, y: 0)
+                }
+                
+                Spacer()
+                
+                // MARK: Player hand
+                ZStack {
+                    CardView(number: 1, suit: 3)
+                    CardView(number: 13, suit: 0)
+                        .offset(x: 50, y: 0)
+                }
+            }
+        }
+        .navigationBarHidden(true)
+        .navigationTitle("")
     }
 }
 

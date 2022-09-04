453ed3b31b871869d3e5151fbb4dea477c40897a
Khoi7 <s3694615@rmit.edu.vn>
2022-09-04 16:06:00 +0700

    add app icon

--- Shared/View/GameView.swift
+++ Shared/View/GameView.swift
@@ -7,6 +7,7 @@
 // <a href="https://www.flaticon.com/free-icons/poker" title="poker icons">Poker icons created by Smashicons - Flaticon</a>
 // <a href="https://www.flaticon.com/free-icons/poker-cards" title="poker cards icons">Poker cards icons created by rizal2109 - Flaticon</a>
 // https://betterprogramming.pub/card-flip-animation-in-swiftui-45d8b8210a00
+// <a href="https://www.flaticon.com/free-icons/blackjack" title="blackjack icons">Blackjack icons created by Good Ware - Flaticon</a>
 
 import SwiftUI
 

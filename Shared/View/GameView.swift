6c2bc15e7e8c8254acfd7e4e51f24d9f6bd5e203
Khoi7 <s3694615@rmit.edu.vn>
2022-09-07 02:43:29 +0700

    add header comments

--- Shared/View/GameView.swift
+++ Shared/View/GameView.swift
@@ -9,14 +9,16 @@
   Last modified: 06/09/2022
   Acknowledgement:
  
-<a href="https://www.flaticon.com/free-icons/poker" title="poker icons">Poker icons created by Smashicons - Flaticon</a>
- 
-<a href="https://www.flaticon.com/free-icons/poker-cards" title="poker cards icons">Poker cards icons created by rizal2109 - Flaticon</a>
- 
-<a href="https://www.flaticon.com/free-icons/blackjack" title="blackjack icons">Blackjack icons created by Good Ware - Flaticon</a>
- 
- https://betterprogramming.pub/card-flip-animation-in-swiftui-45d8b8210a00
- https://betterprogramming.pub/stack-navigation-on-macos-41a40d8ec3a4
+    <a href="https://www.flaticon.com/free-icons/poker" title="poker icons">Poker icons created by Smashicons - Flaticon</a>
+    
+    <a href="https://www.flaticon.com/free-icons/poker-cards" title="poker cards icons">Poker cards icons created by rizal2109 - Flaticon</a>
+    
+    <a href="https://www.flaticon.com/free-icons/blackjack" title="blackjack icons">Blackjack icons created by Good Ware - Flaticon</a>
+    
+    https://betterprogramming.pub/card-flip-animation-in-swiftui-45d8b8210a00
+    
+    https://betterprogramming.pub/stack-navigation-on-macos-41a40d8ec3a4
+    
 */
 
 import SwiftUI
--- Shared/View/HighscoresView.swift
+++ Shared/View/HighscoresView.swift
@@ -1,9 +1,25 @@
-//
-//  HighscoresView.swift
-//  Blackjack (iOS)
-//
-//  Created by Bò Vũ Trụ on 03/09/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Vu Duy Khoi
+  ID: s3694615
+  Created  date: 17/08/2022
+  Last modified: 06/09/2022
+  Acknowledgement:
+ 
+    <a href="https://www.flaticon.com/free-icons/poker" title="poker icons">Poker icons created by Smashicons - Flaticon</a>
+    
+    <a href="https://www.flaticon.com/free-icons/poker-cards" title="poker cards icons">Poker cards icons created by rizal2109 - Flaticon</a>
+    
+    <a href="https://www.flaticon.com/free-icons/blackjack" title="blackjack icons">Blackjack icons created by Good Ware - Flaticon</a>
+    
+    https://betterprogramming.pub/card-flip-animation-in-swiftui-45d8b8210a00
+    
+    https://betterprogramming.pub/stack-navigation-on-macos-41a40d8ec3a4
+    
+*/
 
 import SwiftUI
 
